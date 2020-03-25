a95780dee22e336784a05251b68c92b6cd982889
Tyler Morales <moratyle@gmail.com>
2020-03-24 23:30:29 -0500

    start refactoring and update buttons/ yellow lines

--- assets/js/interactions.js
+++ assets/js/interactions.js
@@ -1,17 +1,16 @@
 // HIDE 'LEARN MORE' LABEL AFTER SCROLL
 const scrollBtn = document.querySelector(".learn-more");
 window.addEventListener("scroll", e => {
-    const scrollBtnY = window.scrollY + scrollBtn.getBoundingClientRect().bottom; // Y
+    const scrollBtnY = window.scrollY + scrollBtn.getBoundingClientRect().bottom
     const scrolled = window.scrollY;
-    console.log(scrolled, scrollBtnY)
+    // console.log(scrolled, scrollBtnY)
 
     if (scrolled > scrollBtnY) {
-        console.log("past btn");
         scrollBtn.style.visibility = 'hidden'
     }
 });
 
-// CONTACT FORM
+// CONTACT FORM 
 // click link below to view form submissions
 //https://docs.google.com/spreadsheets/d/1Yi0d90pNaNWyapom1nR1ccnetbVnAqdxPBlUVL0DRfY/edit?usp=sharing
 
@@ -19,20 +18,21 @@ window.addEventListener("scroll", e => {
 // https://github.com/jamiewilson/form-to-google-sheets
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyWCIvfXHv6kZA_h5gs3Ewr4Tx1_CVKbEk76v9wo_OvlteJx6k/exec'
-const form = document.forms['submit-to-google-sheet']
+const googleForm = document.forms['submit-to-google-sheet']
+const formEl = document.getElementById('form-contact')
 
-form.addEventListener('submit', e => {
+googleForm.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, {
             method: 'POST',
-            body: new FormData(form)
+            body: new FormData(googleForm)
         })
         .then(response => console.log('Success!', response))
         .catch(error => console.error('Error!', error.message))
 })
 
-document.getElementById('form-contact').addEventListener('submit', function () {
-    var button = document.getElementById('submit-button');
+formEl.addEventListener('submit', (e) => {
+    const button = document.getElementById('submit-button');
     button.innerText = 'Message Sent';
     button.disabled = e;
 });
